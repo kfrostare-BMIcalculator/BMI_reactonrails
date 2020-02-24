d48306b1db96d14337aca7c86c9a9db20db7ac15
DanielB <1996.daniel.bryant@gmail.com>
2020-02-24 11:39:56 +0100

    adds requirement to form in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,27 @@ import './App.css';
 
 class App extends Component {
  render() {
-  return 
-    <div>
-     
-    </div>;
+  return  (
+    <form>
+      <label htmlFor="weight">Weight</label>
+      <input
+        type="number"
+        required
+        placeholder="Weight in kgs"
+        name="weight"
+        id="weight"
+    />
+      <label htmlFor="height">Height</label>
+      <input
+        type="number"
+        required
+        placeholderW="Height in cm"
+        name="height"
+        id="height"
+    />
+      <button>Calculate BMI</button>
+    </form>
+  );
  }
 }
 
